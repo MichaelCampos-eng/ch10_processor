5193752464db0e3ea32c42b4d4d1353a99868f17
Michael Campos <michael_c55@berkeley.edu>
2025-03-26 16:16:34 -0600

    Mapped times

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chapter10 import C10
 from chapter10.pcm import PCMF1
+from chapter10.packet import Packet
 from chapter10.computer import ComputerF1
 
 from tmats import GeneralData, Recorder, PCMFormat
@@ -59,7 +60,7 @@ def explore_tmats(saved_path: str):
 
 def extract_tmats(ch10_path: str,
                  save_path: str):
-    with open(save_path, 'wb') as file:
+    with open(save_path, 'w') as file:
         data_json = {}
         for packet in tqdm(C10(ch10_path)):
             if packet.data_type == 0x01:
@@ -69,10 +70,21 @@ def extract_tmats(ch10_path: str,
                 data_json[PJson.pcm_format.value] = PCMFormat.parse(packet=packet)
         json.dump(data_json, file, indent=4)
 
-class TMA:
-    def __init__(self):
+def get_tmats(ch10_path: str) -> FrameInfo:
+    for packet in C10(ch10_path):
+        if packet.data_type == 0x01:
+            packet: ComputerF1 = packet
+            pcm_format = PCMFormat(PCMFormat.parse(packet=packet))
+            return FrameInfo(minor_frame_length = pcm_format.P1MF1,
+                             minor_frame_per_major_frame = pcm_format.P1MFN,
+                             frame_sync_pattern = pcm_format.P1MF5)
+
+class MemorySize:
+    def __init__(self, ch10_path: str, info: FrameInfo):
         self.time_count = {}
         self.frame_count = {}
+        self.__index__ = 0
+        self.__extract__(ch10_path, info)
 
     def incr_tm_cnt(self, channel_id: int):
         if channel_id not in self.time_count.keys():
@@ -83,68 +95,79 @@ class TMA:
         if channel_id not in self.frame_count.keys():
             self.frame_count[channel_id] = 0
         self.frame_count[channel_id] += amount
+    
+    def decr_frm_cnt(self, channel_id: int, amount: int):
+        if channel_id in self.frame_count.keys():
+            self.frame_count[channel_id] -= amount
+        
+    def __extract__(self, ch10_path: str, info: FrameInfo):
+        frame_count = None
+        channel_id = None
+        for packet in tqdm(C10(ch10_path)):
+            channel_id = packet.channel_id
+            self.incr_tm_cnt(channel_id)
+            if packet.data_type == 0x09:
+                packet: PCMF1 = packet
+                frame_count = BodyStream.get_frame_count(packet.buffer.read(), info)
+                self.incr_frm_cnt(channel_id, frame_count)
+        if frame_count and channel_id: # Exclude the last packet
+            self.decr_frm_cnt(channel_id, frame_count) 
 
-def get_tmats(ch10_path: str) -> FrameInfo:
-    for packet in C10(ch10_path):
-        if packet.data_type == 0x01:
-            packet: ComputerF1 = packet
-            pcm_format = PCMFormat(PCMFormat.parse(packet=packet))
-            return FrameInfo(minor_frame_length = pcm_format.P1F1,
-                             minor_frame_per_major_frame = pcm_format.P1MFN,
-                             frame_sync_pattern = pcm_format.P1MF5)
-
-def extract_memory_allocation(ch10_path: str, info: FrameInfo) -> TMA:
-    memory_allocation = TMA()
-    for packet in tqdm(C10(ch10_path)):
-        memory_allocation.incr_tm_cnt(packet.channel_id)
-        if packet.data_type == 0x09:
-            packet: PCMF1 = packet
-            memory_allocation.incr_frm_cnt(packet.channel_id, BodyStream.get_frame_count(packet.buffer.read(), info))
-    return memory_allocation  
-            
-
-def extract_ch10(ch10_path: str,
+def decode_ch10(ch10_path: str,
                  verbose: bool,
                  channel: Optional[int],
                  iterations: Optional[int]):
-    
-    info: FrameInfo = get_tmats(ch10_path)
-    memory_alloc = extract_memory_allocation(ch10_path, info)
-    times = {channel_id: TimeInfo(channel_id=channel_id, memory_size=size) for channel_id, size in memory_alloc.time_count.items()}
 
-    init_time = None
-    prev_time = None
-    payloads = {}
+    info: FrameInfo = get_tmats(ch10_path)
+    payloads: list[int: BodyStream] = {}
+    times: list[int: TimeInfo] = {}
+    memory_alloc = MemorySize(ch10_path, info)
+
+    for channel_id in memory_alloc.time_count.keys():
+        size = memory_alloc.time_count[channel_id]
+        print("Size: {}".format(size))
+        stream_size = None
+        if channel_id in memory_alloc.frame_count.keys():
+            stream_size = memory_alloc.frame_count[channel_id]
+        times[channel_id] = TimeInfo(channel_id, size, stream_size=stream_size)
+
     index = 0
+    offset = None
+    last_packet: Packet = None
     for packet in C10(ch10_path):
-        os.system("clear")
-        print(index)
         if iterations and iterations == index:
+            if channel:
+                print(payloads[packet.channel_id])
             break
-        
+        index += 1
+
         # Assuming tmats is the first packet
         if packet.data_type == 0x01:
             packet: ComputerF1 = packet
-            init_time = packet.get_time()
-            continue
-
-        # Calculate delta times
-        timeinfo = times[packet.channel_id]
-        if prev_time != None:
-            prev_time.append_delta_time(packet.get_time(), init_time)
-        timeinfo.append_time(packet.get_time(), init_time)
-        prev_time = timeinfo
-
-        # Decode PCM body if valid
-        if not timeinfo.is_end() and packet.data_type == 0x09:
-            packet: PCMF1 = packet
-            if not packet.channel_id in payloads.keys():
-                payloads[packet.channel_id] = BodyStream(times[packet.channel_id], info, memory_alloc.frame_count[packet.channel_id])
-            bodystream: BodyStream = payloads[packet.channel_id]
-            bodystream.append_body(packet.buffer.read())
+            offset = packet.get_time()
         
-        index += 1
+        if last_packet != None:
+            # Compute deltas
+            last_timeinfo: TimeInfo = times[last_packet.channel_id]
+            curr_timeinfo: TimeInfo = times[packet.channel_id]
+            curr_timeinfo.append_time(packet.get_time(), offset)
+            last_timeinfo.append_delta_time(packet.get_time(), offset)
 
+            # Decode PCM body, excluding the last packet
+            if last_packet.data_type == 0x09:
+                last_packet: PCMF1 = last_packet
+                # If packet is first in its channel, unable to compute times without dt
+                if not last_packet.channel_id in payloads.keys():
+                    bodystream: BodyStream = BodyStream(last_timeinfo, info, memory_alloc.frame_count[last_packet.channel_id])
+                    bodystream.append_body(last_packet.buffer.read())
+                    payloads[last_packet.channel_id] = bodystream
+                else:
+                    bodystream: BodyStream = payloads[last_packet.channel_id]
+                    bodystream.compute_times()
+                    bodystream.append_body(last_packet.buffer.read())
+
+        last_packet = packet
+                
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(
@@ -153,21 +176,24 @@ if __name__ == "__main__":
         epilog="Testing"
     )
 
-    parser.add_argument('-e', '--extract', default="2025_01_20_ARAV T3_DECOM_4_2_10.ch10")
+    parser.add_argument('-d', '--decode', default="2025_01_20_ARAV T3_DECOM_4_2_10.ch10")
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-c', '--channel_id')
     parser.add_argument('-i', '--iterations')
     parser.add_argument("-s", '--save')
-    parser.add_argument('-t', '--tmats')
+    parser.add_argument('-et', '--extract_tmats')
+    parser.add_argument('-ep', '--explore_tmats')
 
     args = parser.parse_args()
 
-    if args.extract and not args.tmats:
-        extract_ch10(ch10_path= args.extract, 
+    if args.decode and not args.extract_tmats and not args.explore_tmats:
+        decode_ch10(ch10_path= args.decode, 
                      verbose= args.verbose, 
                      channel= int(args.channel_id) if args.channel_id else None,
                      iterations = int(args.iterations) if args.iterations else None)
-    elif args.tmats:
-        explore_tmats(args.tmats)
+    elif args.extract_tmats and not args.explore_tmats:
+        extract_tmats(args.extract_tmats, args.save)
+    elif args.explore_tmats:
+        explore_tmats(args.explore_tmats)
     else:
         parser.error("Missing input! Please input either --extract --save or --parse.")
--- stream.py
+++ stream.py
@@ -1,6 +1,7 @@
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
+from typing import Optional
 
 '''
 Because there is no end time for the last PCM packet, we discard this packet
@@ -14,7 +15,7 @@ class FrameInfo:
                  minor_frame_length: str,
                  minor_frame_per_major_frame: str,
                  frame_sync_pattern: str = '11111110011010110010100001000000'):
-        self.minor_frame_length = int(minor_frame_length)
+        self.minor_frame_length = int(minor_frame_length) + 1
         self.minor_frame_per_major_frame = int(minor_frame_per_major_frame)
         self.frame_sync = FrameInfo.format_frame_sync(frame_sync_pattern)
     
@@ -33,66 +34,82 @@ class FrameInfo:
 
 class TimeInfo:
     
-    def __init__(self, channel_id, memory_size: int):
+    def __init__(self, channel_id: int, time_size: int, stream_size: Optional[int]):
         self.channel_id = channel_id
-        self.__t_stamps__: np.ndarray[np.longdouble] = np.empty((memory_size,), dtype=np.longdouble)
-        self.__delta_times__: np.ndarray[np.longdouble] = np.empty((memory_size - 1,), dtype=np.longdouble)
+        self.__t_stamps__: np.ndarray[np.uint16] = np.empty((time_size,), dtype=np.uint16)
+        self.__delta_times__: np.ndarray[np.uint16] = np.empty((time_size - 1,), dtype=np.uint16)
+        self.times: Optional[np.ndarray[np.uint16]] = np.empty((stream_size, ), np.uint16) if stream_size else None
 
         self.__ts_index__: int = 0
         self.__dt_index__: int = 0
 
-    
     def append_time(self, timestamp: datetime.datetime, offset: datetime.datetime):
         self.__t_stamps__[self.__ts_index__] = (timestamp - offset).total_seconds()
     
-    def append_delta_time(self, endtime: datetime.datetime, offset: datetime.datetime):
-        diff_secs = (endtime - offset).total_seconds() - self.__t_stamps__[self.__ts_index__]
-        self.__delta_times__[self.__dt_index__] = diff_secs
-        self.__dt_index__ += 1
-        self.__ts_index__ += 1
+    def append_delta_time(self, new_time: datetime.datetime, offset: datetime.datetime):
+        if self.__dt_index__ < self.__delta_times__.size:
+            self.__delta_times__[self.__dt_index__] = (new_time - offset).total_seconds() - self.get_ts()
+            self.__dt_index__ += 1
+            self.__ts_index__ += 1
+    
+    # Retrieves the latest dt appended
+    def get_dt(self):
+        return self.__delta_times__[self.__dt_index__ - 1]
 
-    def is_end(self):
-        return self.__dt_index__ >= self.__delta_times__.size - 1
+    # Retrieves current relative timestamp
+    def get_ts(self) -> np.uint16:
+        return self.__t_stamps__[self.__ts_index__]
 
     @property
     def count(self):
-        return len(self.__t_stamps__)
-    
-    def __next__(self):
-        if self.__index__ < self.count:
-            timestamp = self.__t_stamps__[self.__ts_index__]
-            self.__index__ += 1
-            return timestamp
-        else:
-            StopIteration
-    
-    def __iter__(self):
-        self.__ts_index__ = 0
-        return self
+        return self.__delta_times__.size
 
 class BodyStream:
 
     def __init__(self, time_info: TimeInfo, frame_info: FrameInfo, memory: int):
-        self.__timestamps__: TimeInfo = time_info
-        self.__info__: FrameInfo = frame_info
-        self.__data__ = np.empty((frame_info.minor_frame_length, memory - 1), dtype=np.uint8)
+        self.time_info: TimeInfo = time_info
+        self.frame_info: FrameInfo = frame_info
+        self.__frames__ = np.empty((frame_info.minor_frame_length, memory), dtype=np.uint8)
+        self.__delta__: int = None
         self.__index__ = 0
-        self.__frames__ = None
+    
+    def __str__(self):
+        header = "BodyStream for Channel: {}\n".format(self.channel_id)
+        frames_contents = "Number of frames: {}, Frame length: {}\n".format(self.__frames__.shape[1], self.__frames__.shape[0])
+        time_contents = "Number of time deltas: {}\n".format(self.time_info.count)
+        dt_contents = "Number of times: {}\n".format(self.time_info.times.shape)
+        divider = "#" * 50
+        return header + frames_contents + time_contents  + dt_contents + divider
 
     @property
     def channel_id(self):
-        return self.__timestamps__.channel_id
+        return self.time_info.channel_id
     
     def append_body(self, new_data: bytes):
         new_data = np.frombuffer(buffer=new_data, dtype=np.uint8)
-        self.__data__[:,self.__index__] = new_data
-        self.__index__ += 1
+        frames = BodyStream.extract_frames(new_data, self.frame_info)
+        self.__delta__ = frames.shape[1]
+        self.__frames__[:,self.__index__: self.__index__ + self.__delta__] = frames
+        self.__index__ += self.__delta__
 
-    def count(self):
-        return self.__timestamps__.count
+    def compute_times(self):
+        sub_times = np.arange(self.__delta__) * self.time_info.get_dt() / self.__delta__ + self.time_info.get_ts()
+        self.time_info.times[self.__index__ - self.__delta__ : self.__index__] = sub_times
+        
     
-    def create_frames(self, info: FrameInfo):
-        self.__frames__ = BodyStream.extract_frames(info)
+    @staticmethod
+    def extract_frames(data: np.ndarray[np.uint8], info: FrameInfo) -> np.ndarray[np.uint16]:
+        potentials_indices = {}
+        for i in range(info.byte_width):
+            aligned = BodyStream.__align__(data, i, info.byte_width)
+            potentials_indices[i] = BodyStream.__find_sfp_indices__(aligned, info.frame_sync)
+        shift, indices = max(potentials_indices.items(), key=lambda x: len(x[1]))
+        indices = indices[indices >= info.frame_byte_length]
+        aligned = BodyStream.__align__(data, shift, info.byte_width)
+        frames = np.empty((info.minor_frame_length, len(indices)), dtype=np.uint16)
+        for count, index in enumerate(indices):
+            frames[:, count] = aligned[index - info.frame_byte_length + 4: index + 4].astype(np.uint8).view('<u2')
+        return frames
 
     @staticmethod
     def __find_sfp_indices__(main_list: np.ndarray[np.uint8], sub_list: np.ndarray[np.uint8]) -> np.ndarray[np.uint8]:
@@ -109,22 +126,8 @@ class BodyStream:
         return np.bitwise_or(left_shift, right_shift)
     
     @staticmethod
-    def __extract_frames__(data: np.ndarray[np.uint8], info: FrameInfo) -> np.ndarray[np.uint16]:
-        potentials_indices = {}
-        for i in range(info.byte_width):
-            aligned = BodyStream.__align__(data, i, info.byte_width)
-            potentials_indices[i] = BodyStream.__find_sfp_indices__(aligned, info.frame_sync)
-        shift, indices = max(potentials_indices.items(), key=lambda x: len(x[1]))
-        indices = indices[indices >= info.frame_byte_length]
-        aligned = BodyStream.__align__(data, shift, info.byte_width)
-        frames = np.empty((info.minor_frame_length, len(indices)), dtype=np.uint16)
-        for count, index in enumerate(indices):
-            frames[:, count] = aligned[index - info.frame_byte_length + 4: index + 4].astype(np.uint8).view('<u2')
-        return frames
-    
-    @staticmethod
     def get_frame_count(data: bytes, info: FrameInfo):
-        return  BodyStream.__extract_frames__(np.frombuffer(data, np.uint8), info).shape[1]
+        return  BodyStream.extract_frames(np.frombuffer(data, np.uint8), info).shape[1]
         
     @staticmethod
     def save_stream(stream: np.ndarray[np.uint8]):
